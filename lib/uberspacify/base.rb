810619d8f265869875daf48c43dfe08302d5d0fc
Alexei Matveev <alex@telehype.org>
2014-02-20 16:56:34 +0100

    Revert "-- remove rvm"
    This reverts commit 58c2065c74186162aa697da7f9ef819b5a1ffa7a.

--- lib/uberspacify/base.rb
+++ lib/uberspacify/base.rb
@@ -1,5 +1,6 @@
 require 'capistrano_colors'
-require 'capistrano/bundler'
+require 'rvm/capistrano'
+require 'bundler/capistrano'
 
 def abort_red(msg)
   abort "  * \e[#{1};31mERROR: #{msg}\e[0m"
@@ -25,16 +26,16 @@ Capistrano::Configuration.instance.load do
   set(:deploy_to)               { "/var/www/virtual/#{user}/rails/#{application}" }
   set(:home)                    { "/home/#{user}" }
   set(:use_sudo)                { false }
-  # set(:rvm_type)                { :user }
-  # set(:rvm_install_ruby)        { :install }
-  # set(:rvm_ruby_string)         { "ree@rails-#{application}" }
+  set(:rvm_type)                { :user }
+  set(:rvm_install_ruby)        { :install }
+  set(:rvm_ruby_string)         { "ree@rails-#{application}" }
 
   ssh_options[:forward_agent] = true
   default_run_options[:pty]   = true
 
   # callbacks
-  # before  'deploy:setup',           'rvm:install_rvm'
-  # before  'deploy:setup',           'rvm:install_ruby'
+  before  'deploy:setup',           'rvm:install_rvm'
+  before  'deploy:setup',           'rvm:install_ruby'
   after   'deploy:setup',           'uberspace:setup_svscan'
   after   'deploy:setup',           'daemontools:setup_daemon'
   after   'deploy:setup',           'apache:setup_reverse_proxy'
@@ -55,7 +56,7 @@ Capistrano::Configuration.instance.load do
 export HOME=#{fetch :home}
 source $HOME/.bash_profile
 cd #{fetch :deploy_to}/current
-# rvm use #{fetch :rvm_ruby_string}
+rvm use #{fetch :rvm_ruby_string}
 exec bundle exec passenger start -p #{fetch :passenger_port} -e production 2>&1
       EOF
 
